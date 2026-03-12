231d620f11db9e6754056f05cf4df4325d0c899b
covo43k <covo4ek42@gmail.com>
2026-03-12 09:25:46 +0000

    Redacted Lab2

--- Lab2/Task3/richard.cpp
+++ Lab2/Task3/richard.cpp
@@ -29,14 +29,14 @@ double cpuSecond()
 }
 
 //###############################################РИЧАРДСОН SERIAL########################################################################
-void richardson_serial(const std::vector<double>& A, const std::vector<double>& b, std::vector<double>& x, int n)
+void richardson_serial(const std::vector<double>& A, const std::vector<double>& b, std::vector<double>& x, int n, double b_norm)
 {
     const double tau = 0.01;
     int iter = 0;
-    double norm = 1.0;
+    double residual_norm = 1.0;
 
-    while (iter < MAX_ITER && norm > EPS){
-        norm = 0.0;
+    while (iter < MAX_ITER && (residual_norm / b_norm) > EPS){
+        residual_norm = 0.0;
         for (int i = 0; i < n; i++){
 
             double Ax = 0.0;
@@ -46,20 +46,20 @@ void richardson_serial(const std::vector<double>& A, const std::vector<double>& 
 
             double r = b[i] - Ax;
             x[i] += tau * r;
-            norm += r * r;
+            residual_norm += r * r;
         }
-        norm = std::sqrt(norm);
+        residual_norm = std::sqrt(residual_norm);
         iter++;
     }
-    std::cout << "Метод простой итерации (serial) завершился за " << iter << " итераций, норма = " << std::scientific << norm << "\n";
+    std::cout << "Метод простой итерации (serial) завершился за " << iter << " итераций, норма = " << std::scientific << (residual_norm / b_norm) << "\n";
 }
 
-void run_serial(const std::vector<double>& A, const std::vector<double>& b, int n)
+void run_serial(const std::vector<double>& A, const std::vector<double>& b, int n, double b_norm)
 {
     std::vector<double> x(n, 0.0);
 
     double t = cpuSecond();
-    richardson_serial(A, b, x, n);
+    richardson_serial(A, b, x, n, b_norm);
     t = cpuSecond() - t;
 
     double max_err = 0.0;
@@ -76,14 +76,14 @@ void run_serial(const std::vector<double>& A, const std::vector<double>& b, int 
 
 
 //###############################################РИЧАРДСОН SEPARATE########################################################################
-void richardson_omp_separate(const std::vector<double>& A, const std::vector<double>& b, std::vector<double>& x, int n)
+void richardson_omp_separate(const std::vector<double>& A, const std::vector<double>& b, std::vector<double>& x, int n, double b_norm)
 {
     const double tau = 0.01;
     int iter = 0;
-    double norm = 1.0;
+    double residual_norm = 1.0;
 
-    while (iter < MAX_ITER && norm > EPS){
-        norm = 0.0;
+    while (iter < MAX_ITER && (residual_norm / b_norm) > EPS){
+        residual_norm = 0.0;
 
 #pragma omp parallel for num_threads(NUM_THREADS)
         for (int i = 0; i < n; i++){
@@ -105,21 +105,21 @@ void richardson_omp_separate(const std::vector<double>& A, const std::vector<dou
             double diff = r * r;
 
 #pragma omp atomic
-            norm += diff;
+            residual_norm += diff;
         }
 
-        norm = std::sqrt(norm);
+        residual_norm = std::sqrt(residual_norm);
         iter++;
     }
-    std::cout << "Метод простой итерации (separate parallel for) завершился за " << iter << " итераций, норма = " << std::scientific << norm << "\n";
+    std::cout << "Метод простой итерации (separate parallel for) завершился за " << iter << " итераций, норма = " << std::scientific << (residual_norm / b_norm) << "\n";
 }
 
-void run_omp_separate(const std::vector<double>& A, const std::vector<double>& b, int n)
+void run_omp_separate(const std::vector<double>& A, const std::vector<double>& b, int n, double b_norm)
 {
     std::vector<double> x(n, 0.0);
 
     double t = cpuSecond();
-    richardson_omp_separate(A, b, x, n);
+    richardson_omp_separate(A, b, x, n, b_norm);
     t = cpuSecond() - t;
 
     double max_err = 0.0;
@@ -135,14 +135,14 @@ void run_omp_separate(const std::vector<double>& A, const std::vector<double>& b
 
 
 //###############################################РИЧАРДСОН ONE########################################################################
-void richardson_omp_one(const std::vector<double>& A, const std::vector<double>& b, std::vector<double>& x, int n)
+void richardson_omp_one(const std::vector<double>& A, const std::vector<double>& b, std::vector<double>& x, int n, double b_norm)
 {
     const double tau = 0.01;
     int iter = 0;
-    double norm = 1.0;
+    double residual_norm = 1.0;
 
-    while (iter < MAX_ITER && norm > EPS){
-        norm = 0.0;
+    while (iter < MAX_ITER && (residual_norm / b_norm) > EPS){
+        residual_norm = 0.0;
 
 #pragma omp parallel num_threads(NUM_THREADS)
        {
@@ -166,20 +166,20 @@ void richardson_omp_one(const std::vector<double>& A, const std::vector<double>&
             }
 
             #pragma omp atomic
-            norm += norm_local;
+            residual_norm += norm_local;
         }
-        norm = std::sqrt(norm);
+        residual_norm = std::sqrt(residual_norm);
         iter++;
     }
-    std::cout << "Метод простой итерации (один parallel) завершился за " << iter << " итераций, норма = " << std::scientific << norm << "\n";
+    std::cout << "Метод простой итерации (один parallel) завершился за " << iter << " итераций, норма = " << std::scientific << (residual_norm / b_norm) << "\n";
 }
 
-void run_omp_one(const std::vector<double>& A, const std::vector<double>& b, int n)
+void run_omp_one(const std::vector<double>& A, const std::vector<double>& b, int n, double b_norm)
 {
     std::vector<double> x(n, 0.0);
 
     double t = cpuSecond();
-    richardson_omp_one(A, b, x, n);
+    richardson_omp_one(A, b, x, n, b_norm);
     t = cpuSecond() - t;
 
     double max_err = 0.0;
@@ -196,16 +196,16 @@ void run_omp_one(const std::vector<double>& A, const std::vector<double>& b, int
 
 
 //###############################################РИЧАРДСОН С SCHEDULE########################################################################
-void richardson_omp_schedule(const std::vector<double>& A, const std::vector<double>& b, std::vector<double>& x, int n, const char* schedule_name)
+void richardson_omp_schedule(const std::vector<double>& A, const std::vector<double>& b, std::vector<double>& x, int n, const char* schedule_name, double b_norm)
 {
     const double tau = 0.01;
     int iter = 0;
-    double norm = 1.0;
+    double residual_norm = 1.0;
 
     std::cout << "schedule = " << schedule_name << std::endl;
 
-    while (iter < MAX_ITER && norm > EPS) {
-        norm = 0.0;
+    while (iter < MAX_ITER && (residual_norm / b_norm) > EPS) {
+        residual_norm = 0.0;
 
 #pragma omp parallel for num_threads(NUM_THREADS) schedule(runtime)
         for (int i = 0; i < n; i++) {
@@ -225,21 +225,21 @@ void richardson_omp_schedule(const std::vector<double>& A, const std::vector<dou
             }
             double diff = r * r;
 #pragma omp atomic
-            norm += diff;
+            residual_norm += diff;
         }
 
-        norm = std::sqrt(norm);
+        residual_norm = std::sqrt(residual_norm);
         iter++;
     }
     std::cout << "Завершился за " << iter << " итераций\n";
 }
 
-void run_schedule_test(const std::vector<double>& A, const std::vector<double>& b, int n, const char* schedule_name)
+void run_schedule_test(const std::vector<double>& A, const std::vector<double>& b, int n, const char* schedule_name, double b_norm)
 {
     std::vector<double> x(n, 0.0);
 
     double t = cpuSecond();
-    richardson_omp_schedule(A, b, x, n, schedule_name);
+    richardson_omp_schedule(A, b, x, n, schedule_name, b_norm);
     t = cpuSecond() - t;
 
     double max_err = 0.0;
@@ -269,27 +269,31 @@ int main(int argc, char** argv)
         b[i] = n + 1;
     }
 
+    double b_norm = 0.0;
+    for (double val : b) b_norm += val * val;
+    b_norm = std::sqrt(b_norm);
+
     std::cout << "Решение системы Ax=b методом простой итерации (Ричардсона)\n";
     std::cout << "Размерность N = " << n << ", MAX_ITER = " << MAX_ITER << ", EPS = " << EPS << "N_THREADS = " << NUM_THREADS << "\n\n";
 
     //run_serial(A, b, n);
-    run_omp_separate(A, b, n);
-    run_omp_one(A, b, n);
+    run_omp_separate(A, b, n, b_norm);
+    run_omp_one(A, b, n, b_norm);
 
     
     std::cout << "=== Исследование schedule для метода Ричардсона ===\n";
     std::cout << "N = " << n << " | Потоков = " << NUM_THREADS << "\n\n";
 
-    run_schedule_test(A, b, n, "static");
-    run_schedule_test(A, b, n, "static,1");
-    run_schedule_test(A, b, n, "static,32");
-    run_schedule_test(A, b, n, "static,128");
-    run_schedule_test(A, b, n, "dynamic");
-    run_schedule_test(A, b, n, "dynamic,8");
-    run_schedule_test(A, b, n, "dynamic,32");
-    run_schedule_test(A, b, n, "guided");
-    run_schedule_test(A, b, n, "guided,16");
-    run_schedule_test(A, b, n, "auto");
+    run_schedule_test(A, b, n, "static", b_norm);
+    run_schedule_test(A, b, n, "static,1", b_norm);
+    run_schedule_test(A, b, n, "static,32", b_norm);
+    run_schedule_test(A, b, n, "static,128", b_norm);
+    run_schedule_test(A, b, n, "dynamic", b_norm);
+    run_schedule_test(A, b, n, "dynamic,8", b_norm);
+    run_schedule_test(A, b, n, "dynamic,32", b_norm);
+    run_schedule_test(A, b, n, "guided", b_norm);
+    run_schedule_test(A, b, n, "guided,16", b_norm);
+    run_schedule_test(A, b, n, "auto", b_norm);
 
     return 0;
 }
